7bdc9dc105581a49d98be6a28f2a1978f45d0b9d
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-09-12 23:45:45 +0800

    Update Cat.java

--- factoryPattern/Cat.java
+++ factoryPattern/Cat.java
@@ -3,10 +3,6 @@ package factoryPattern;
 public class Cat implements Pet {
     private Integer noOfLives;
 
-    public Cat() {
-        this.noOfLives = 9;  // Cats typically have 9 lives.
-    }
-
     public Integer getNoOfLives() {
         return noOfLives;
     }
@@ -23,9 +19,5 @@ public class Cat implements Pet {
     @Override
     public String play() {
         return "Catching/pursuing moving objects!";
-    }
-
-    public String showLives() {
-        return "The cat has " + noOfLives + " lives.";
     }
 }
